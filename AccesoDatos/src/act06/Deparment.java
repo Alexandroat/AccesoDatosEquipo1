27c992c2be24ed4b008ee98f74fd78b590e4f9cb
Alejandro <fj@10.34.83.103>
2014-10-30 17:14:13 +0100

    commit corregido

---
+++ AccesoDatos/src/act06/Deparment.java
@@ -0,0 +1,22 @@
+package act06;
+import java.io.Serializable;
+
+public class Deparment {
+
+	private static final long serialVersionUID = 1L;
+
+	public Deparment() {
+
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		return super.equals(obj);
+	}
+
+	@Override
+	public String toString() {
+		return super.toString();
+	}
+
+}
