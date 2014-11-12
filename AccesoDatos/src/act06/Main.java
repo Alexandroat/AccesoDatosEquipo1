db020001d5295f255380b43924c80ec2b4542313
Alejandro <fj@10.34.83.180>
2014-11-12 15:47:10 +0100

    commit 6

--- AccesoDatos/src/act06/Main.java
+++ AccesoDatos/src/act06/Main.java
@@ -5,10 +5,20 @@ import java.util.Date;
 public class Main {
 
 	public static void main(String[] args) {
-		Employee e = new Employee(01, "pepinezdqqwdfewqhqkq56elhgerslgeger", "Albelrt", "lavatapers",
-				56, 23, 4352, 5);
+		Employee e = new Employee(01, "Perez", "Rodrigo", "Trabajo",
+				"12/12/2012", 3000, 29, 9);
+		Employee e2 = new Employee(02, "Esteban", "Benito", "NTrabajo",
+				"11/12/2010", 200, 12, 5);
+		Employee e3 = new Employee(04, "Aster", "Ast", "Strabajo",
+				"01/01/1900", 3400, 34, 52);
+		
+		EmployeeManager em = new EmployeeManager();
+		
+		em.addEmployee(e);
+		em.addEmployee(e2);
+		em.listEmployee(e3);
 	
-		System.out.println(e.getLastName());
+		//System.out.println(e.getLastName());
 
 	}
 
